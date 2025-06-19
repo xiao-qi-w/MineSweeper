36d959701fa5387682468fa93a06fa7ae32280b2
jiaqi3.guo <jiaqi3.guo@cotticoffee.com>
2025-06-19 09:57:10 +0800

    删除.idea文件夹

--- src/Main.java
+++ src/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
-            // 加载布局文件
+            // 加载布局文件, 初始化
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/fxmls/menu.fxml"));
             Parent root = loader.load();
